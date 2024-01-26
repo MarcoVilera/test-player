774d252fd192329bfc89349d6cf53a6265c78441
Marco Vilera <MarcoVilArev123@gmail.com>
2024-01-25 21:09:31 -0400

    modify function structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import './App.css'
-import { Playing } from './components/playing'
-import { Panel } from './components/panel'
+import "./App.css"
+import { Playing } from "./components/playing"
+import { Panel } from "./components/panel"
+import { Buttons } from "./components/buttons"
 function App() {
-
-  return (
-    <>
-      <Panel/>
-      <Playing title="title" artist="artist"/>
-    </>
-  )
+    return (
+        <>
+            <Panel />
+            <Playing title="title" artist="artist" />
+            <Buttons />
+        </>
+    )
 }
 
 export default App
